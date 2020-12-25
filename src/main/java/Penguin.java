10bbf708e1926197cec91098c20c018951936b33
Pratik Valvi <pratikvalvi2305@gmail.com>
2020-12-25 13:16:21 +0530

    Two new classes added ToyBird and Penguin

--- src/main/java/Penguin.java
+++ src/main/java/Penguin.java
@@ -1,3 +1,17 @@
-public class Penguin {
+public class Penguin implements Birds {
 
+    @Override
+    public void fly() {
+
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(Penguin.class.getName() + " is eating");
+    }
+
+    @Override
+    public void speak() {
+        System.out.println(Penguin.class.getName() + " is speaking");
+    }
 }
